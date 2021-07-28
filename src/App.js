d75f6baae578fe4324b485daac03fb7dd0dee021
Remigiusz Kolanko <rkolanko@workforcesoftware.com>
2021-07-28 16:12:36 +0200

    Responsive for header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { Header } from "./Header/Header";
 
 function App() {
