98c57ffbc1519f3a4178a050ad0c6c98632998c3
Remigiusz Kolanko <rkolanko@workforcesoftware.com>
2021-07-29 09:34:55 +0200

    Footer styling

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,113 @@
 import "./Footer.css";
+import logo from "../assets/images/logo.png";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 
 export const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <footer className="footer">
+      <Row>
+        <Col xs="12" md="6">
+          <a href="#" title="Office Suplies">
+            <img src={logo} className="logo" alt="logo" />
+          </a>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+            accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
+            Cum sociis natoque penatibus et magnis dis parturient montes,
+            nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra
+            vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget
+            odio.
+          </p>
+        </Col>
+        <Col xs="12" md="3">
+          <h3 className="footer-header">Bestsellers</h3>
+          <Row>
+            <Col xs="12" md="6">
+              <ul className="footer-list">
+                <li>
+                  <a href="#" title="A4">
+                    <span>A4</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="A5">
+                    <span>A5</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Pens">
+                    <span>Pens</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Staplers">
+                    <span>Staplers</span>
+                  </a>
+                </li>
+              </ul>
+            </Col>
+            <Col xs="12" md="6">
+              <ul className="footer-list">
+                <li>
+                  <a href="#" title="A4">
+                    <span>A4</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="A5">
+                    <span>A5</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Pens">
+                    <span>Pens</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Staplers">
+                    <span>Staplers</span>
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs="12" md="3">
+          <h3 className="footer-header">We're social!</h3>
+          <ul className="footer-social-list">
+            <li>
+              <a href="#" title="Facebook">
+                <FontAwesomeIcon
+                  className="footer-social-list-icon"
+                  icon={faShoppingBasket}
+                />
+                <span>Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" title="Twitter">
+                <FontAwesomeIcon
+                  className="footer-social-list-icon"
+                  icon={faShoppingBasket}
+                />
+                <span>Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" title="LinkedIn">
+                <FontAwesomeIcon
+                  className="footer-social-list-icon"
+                  icon={faShoppingBasket}
+                />
+                <span>LinkedIn</span>
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </footer>
+  );
 };
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -15,65 +15,64 @@ import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = () => {
   return (
-      <Navbar className="d-block" bg="light" expand="lg">
-        <Row>
-          <Col xs={3} className="d-xs-block d-md-none">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          </Col>
-          <Col xs="6" md="3">
-            <Navbar.Brand href="#">
-              <img src={logo} className="logo" alt="logo" />
-            </Navbar.Brand>
-          </Col>
-          <Col md={4} className="d-none d-md-block">
-            <Form className="d-flex header-form">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="mr-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Col>
-          <Col xs={3} md={3} className="header-basket">
-            <div className="d-flex"> 
-              <FontAwesomeIcon
-                className="header-basket-icon"
-                icon={faShoppingBasket}
-              />
-              <Badge className="header-basket-badge" bg="secondary">
-                9
-              </Badge>
-              <span className="header-basket-price d-none d-md-block">565,00 kr</span>
-            </div>
-          </Col>
-          <Col md={2} className="d-none d-md-block">
-            <Button
-              className="header-checkout-button"
-              variant="outline-danger"
-            >
-              Check out
-            </Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-                <Nav.Link title="All our products" href="#">
-                  All our products
-                </Nav.Link>
-                <Nav.Link title="Inspiration for the office" href="#">
-                  Inspiration for the office
-                </Nav.Link>
-                <Nav.Link title="About OS" href="#">
-                  About OS
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Col>
-        </Row>
-      </Navbar>
+    <Navbar className="d-block" bg="light" expand="lg">
+      <Row>
+        <Col xs={3} className="d-xs-block d-md-none">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </Col>
+        <Col xs="6" md="3">
+          <Navbar.Brand href="#">
+            <img src={logo} className="logo" alt="logo" />
+          </Navbar.Brand>
+        </Col>
+        <Col md={4} className="d-none d-md-block">
+          <Form className="d-flex header-form">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="mr-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Col>
+        <Col xs={3} md={3} className="header-basket">
+          <div className="d-flex">
+            <FontAwesomeIcon
+              className="header-basket-icon"
+              icon={faShoppingBasket}
+            />
+            <Badge className="header-basket-badge" bg="secondary">
+              9
+            </Badge>
+            <span className="header-basket-price d-none d-md-block">
+              565,00 kr
+            </span>
+          </div>
+        </Col>
+        <Col md={2} className="d-none d-md-block">
+          <Button className="header-checkout-button" variant="outline-danger">
+            Check out
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link title="All our products" href="#">
+                All our products
+              </Nav.Link>
+              <Nav.Link title="Inspiration for the office" href="#">
+                Inspiration for the office
+              </Nav.Link>
+              <Nav.Link title="About OS" href="#">
+                About OS
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Col>
+      </Row>
+    </Navbar>
   );
 };
