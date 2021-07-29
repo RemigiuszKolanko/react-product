070912dd1c123e83a88a430c66efb7ae2d30199e
Remigiusz Kolanko <rkolanko@workforcesoftware.com>
2021-07-29 10:02:16 +0200

    Fix for css

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,21 +1,19 @@
 import "./Sidebar.css";
 
-export const Sidebar = ({sidebarMenu}) => {
-  console.log('sidebarMenu', sidebarMenu);
+export const Sidebar = ({ sidebarMenu }) => {
+  console.log("sidebarMenu", sidebarMenu);
   return (
     <nav>
       <ul className="sidebar-list">
-        {
-          sidebarMenu.map((element, index) => {
-            return (
-              <li key={index}>
-          <a href="#" title={element}>
-            <span>{element}</span>
-          </a>
-        </li>
-            )
-          })
-        }
+        {sidebarMenu.map((element, index) => {
+          return (
+            <li key={index}>
+              <a href="#" title={element}>
+                {element}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
